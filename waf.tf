c1ba819e6098283feb8f380dd9283b6634e1d96b
Ahsan Kazmi <50926656+kazmithub@users.noreply.github.com>
2026-04-20 17:39:52 +0500

    feat: add WAF v2 with managed rules and rate limiting for ALB protection

---
+++ waf.tf
@@ -0,0 +1,89 @@
+# AWS WAF v2 - Web Application Firewall for ALB
+# Protects against common web exploits and bots
+
+resource "aws_wafv2_web_acl" "main" {
+  name        = "weblate-waf"
+  description = "WAF rules for Weblate ALB"
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CommonRuleSetMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "KnownBadInputsMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "RateLimitRule"
+    priority = 3
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 2000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "RateLimitMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "WeblateWAFMetric"
+    sampled_requests_enabled   = true
+  }
+}
+
+# resource "aws_wafv2_web_acl_association" "main" {
+#   resource_arn = aws_lb.main.arn
+#   web_acl_arn  = aws_wafv2_web_acl.main.arn
+# }
